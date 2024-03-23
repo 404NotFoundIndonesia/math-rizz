aea4040b35a59d8ef354be05f27bea0123777e85
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-03-23 09:43:34 +0800

    separate footer component

---
+++ resources/js/Pages/Components/Footer.svelte
@@ -0,0 +1,14 @@
+<section id="footer">
+    <div class="container text-light mb-5">
+        <hr class="mb-5">
+        <div class="row w-100">
+            <div class="col-12 col-md-5">
+                <div class="">
+                    <h3 class="poppins-light">Math Rizz</h3>
+                    <p class="poppins-extralight">Math Rizz bertujuan untuk merevolusi pendidikan matematika bagi siswa SMA dengan menyediakan lingkungan belajar yang dinamis dan interaktif. Ini memberdayakan siswa untuk unggul dalam matematika dan mempersiapkan mereka untuk sukses akademis dan masa depan yang gemilang.</p>
+                </div>
+            </div>
+        </div>
+        <p class="mt-5">Copyright © 2024 404 Not Found Indonesia</p>
+    </div>
+</section>
--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Footer from "./Components/Footer.svelte";
 
 </script>
 
@@ -41,20 +42,7 @@
     </div>
 </section>
 
-<section id="footer">
-    <div class="container text-light mb-5">
-        <hr class="mb-5">
-        <div class="row w-100">
-            <div class="col-12 col-md-5">
-                <div class="">
-                    <h3 class="poppins-light">Math Rizz</h3>
-                    <p class="poppins-extralight">Math Rizz bertujuan untuk merevolusi pendidikan matematika bagi siswa SMA dengan menyediakan lingkungan belajar yang dinamis dan interaktif. Ini memberdayakan siswa untuk unggul dalam matematika dan mempersiapkan mereka untuk sukses akademis dan masa depan yang gemilang.</p>
-                </div>
-            </div>
-        </div>
-        <p class="mt-5">Copyright © 2024 404 Not Found Indonesia</p>
-    </div>
-</section>
+<Footer />
 
 <style>
     section#pathway {
