436782e4451479ef504ed3a4b6bbc86a5a1b3152
M. Iqbal Effendi <iqbaleff214@gmail.com>
2024-03-30 03:27:35 +0800

    update CEO title and description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="author" content="404 Not Found Indonesia">
     <meta
-        content="Math Rizz"
+        content="Kita usahakan matematika yang menyenangkan itu"
         name="description">
     <meta content="elearning highschool,math rizz,math-rizz,math_rizz,404 not found indonesia,elearning math,404nf_id" name="keywords">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="{{ asset('logo.jpeg') }}"/>
 
-    <title>E-Learning Fungsi Kuadrat</title>
+    <title>Math Rizz</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <title></title>
     @vite('resources/js/app.js')
